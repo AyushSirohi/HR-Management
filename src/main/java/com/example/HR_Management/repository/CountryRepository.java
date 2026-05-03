d6b7ddf81cb9342c7af14f3b9fbfab142821055b
Tithaye Beri <tithaye.beri@gmail.com>
2026-05-03 21:39:55 +0530

    removed controller, modified country repo and employee repo

--- src/main/java/com/example/HR_Management/repository/CountryRepository.java
+++ src/main/java/com/example/HR_Management/repository/CountryRepository.java
@@ -10,16 +10,12 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import com.example.HR_Management.entity.Country;
 import com.example.HR_Management.projection.CountryProjection;
 
-@RepositoryRestResource(path = "countries", excerptProjection = CountryProjection.class)
+@RepositoryRestResource(path = "countries")
 public interface CountryRepository extends JpaRepository<Country, String> {
 
-	
-    //boolean existsByCountryNameIgnoreCase(String countryName);
 	Optional<Country> findByCountryNameIgnoreCase(String countryName);
 	 
-    // Find all countries belonging to a specific region
     List<Country> findByRegion_RegionId(BigDecimal regionId);
  
-    // Check whether a country name already exists
     boolean existsByCountryNameIgnoreCase(String countryName);
 }
